8d14a318bb34ae0eb5e16115383b2cf3ff1793f6
utkugzl <utku.guzel44@gmail.com>
2024-02-05 16:34:49 +0300

    sale screen filtering products feature complated

--- src/utils/ScaleByHeight.js
+++
@@ -1,9 +0,0 @@
-import {Dimensions} from 'react-native';
-
-const {height} = Dimensions.get('window');
-
-const scaleByHeight = percentage => {
-  return height * (parseFloat(percentage) / 100);
-};
-
-export default scaleByHeight;
--- src/utils/ScaleByWidth.js
+++
@@ -1,9 +0,0 @@
-import {Dimensions} from 'react-native';
-
-const {width} = Dimensions.get('window');
-
-const scaleByWidth = percentage => {
-  return width * (parseFloat(percentage) / 100);
-};
-
-export default scaleByWidth;
